fc3008e4c681bc74b6112d37ff3cdb84309a8257
M Tayyab Naeem <mtayyabnaeem@live.com>
2020-07-27 14:36:33 -0700

    Added parsing code; Added ScrollView; Image displays, but parsed textViews still don't.

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,23 +4,35 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
+import org.json.JSONException;
+
+
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
 
+    TextView getDescription, getOrigin,getAlsoKnownAs,ingredientsTv;
+    Sandwich sandwich;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
         ImageView ingredientsIv = findViewById(R.id.image_iv);
+        getDescription = findViewById(R.id.description_tv);
+        getOrigin = findViewById(R.id.origin_tv);
+        getAlsoKnownAs = findViewById(R.id.also_known_tv);
+        ingredientsTv = findViewById(R.id.ingredients_tv);
+
+
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -36,7 +48,12 @@ public class DetailActivity extends AppCompatActivity {
 
         String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
         String json = sandwiches[position];
-        Sandwich sandwich = JsonUtils.parseSandwichJson(json);
+        Sandwich sandwich = null;
+        try {
+            sandwich = JsonUtils.parseSandwichJson(json);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
         if (sandwich == null) {
             // Sandwich data unavailable
             closeOnError();
@@ -58,4 +75,7 @@ public class DetailActivity extends AppCompatActivity {
     private void populateUI() {
 
     }
+
+
+
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,68 @@
 package com.udacity.sandwichclub.utils;
 
+import android.util.Log;
+
 import com.udacity.sandwichclub.model.Sandwich;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class JsonUtils {
 
-    public static Sandwich parseSandwichJson(String json) {
-        return null;
+    private static final String TAG = JsonUtils.class.getName();
+
+    public static Sandwich parseSandwichJson(String json) throws JSONException {
+        final String keyName = "name";
+        final String keyPlaceOfOrigin = "placeOfOrigin";
+        final String keyDescription = "description";
+        final String keyImage = "image";
+        final String keyMainName = "mainName";
+        final String keyIngredients = "ingredients";
+        final String keyAlsoKnowAs= "alsoKnownAs";
+
+        JSONObject sandwichObject = new JSONObject(json);
+        //get the name JSONObject
+        JSONObject name = sandwichObject.getJSONObject(keyName);
+
+        //get origin
+        String origin = sandwichObject.getString(keyPlaceOfOrigin);
+        //get description
+        String description = sandwichObject.getString(keyDescription);
+        //get image
+        String image = sandwichObject.getString(keyImage);
+        //get nameString
+        String nameString = name.getString(keyMainName);
+
+        //i will create method getArrayObject since our data structure is in a List of type string to get the ingredients and alsoknowas.
+        List<String> ingredients = getArrayObject(sandwichObject.getJSONArray(keyIngredients));
+
+        List<String> asKnowAsName = getArrayObject(name.getJSONArray(keyAlsoKnowAs));
+
+        // return the Sandwich class which takes six parameters.
+        return new Sandwich(nameString, asKnowAsName, origin, description, image, ingredients);
+
     }
+
+    private static List<String> getArrayObject(JSONArray jsonArray) {
+        List<String> list = new ArrayList<>();
+        try {
+            int n = jsonArray.length();
+            for (int i = 0; i < n; i++) {
+                list.add(jsonArray.getString(i));
+            }
+
+        } catch (JSONException e ){
+
+            Log.e(TAG,"Error parsing",e);
+
+        }
+        return list;
+    }
+
+
+
 }
